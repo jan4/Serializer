7a84cab4e2eae5c3d51cdc65ba1a5553991f3dfb
Simon Gene Gottlieb <gene@staubsaal.de>
2015-11-01 23:46:19 +0100

    adding convert function for units

--- src/serializer/serializer.h
+++ src/serializer/serializer.h
@@ -4,3 +4,7 @@
 #include "JsonSerializer.h"
 #include "standardTypes.h"
 
+#ifdef ABUILD_UNITS
+	#include "unitTypes.h"
+#endif
+
---
+++ src/serializer/unitTypes.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <units/units.h>
+
+namespace serializer {
+	template<typename T>
+	class Converter<Units::StrongDouble<T>> {
+	public:
+		template<typename Adapter>
+		static void serialize(Adapter& adapter, Units::StrongDouble<T>& x) {
+			double v = x.value();
+			adapter.serialize(v);
+		}
+		template<typename Adapter>
+		static void deserialize(Adapter& adapter, Units::StrongDouble<T>& x) {
+			double v;
+			adapter.deserialize(v);
+			x = v * Units::StrongDouble<T>::one();
+		}
+	};
+}
+
